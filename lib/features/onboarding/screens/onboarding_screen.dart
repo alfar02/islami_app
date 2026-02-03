6eaaf7d576f045462ab0fe2feab85c809e12b704
alfar02 <fe3365870@gmail.com>
2026-02-03 10:05:05 +0200

    feat: Enhance onboarding screen styling and structure

--- lib/features/onboarding/screens/onboarding_screen.dart
+++ lib/features/onboarding/screens/onboarding_screen.dart
@@ -16,9 +16,24 @@ class OnBoardingScreen extends StatelessWidget {
         globalBackgroundColor: const Color(0xff202020),
         pages: onboardingItems.map(buildOnBoardingPage).toList(),
         showBackButton: true,
-        back: const Text("Back"),
+        back: Text(
+          "Back",
+          style: TextStyle(fontSize: 18.sp, color: Colors.amber),
+        ),
         next: Icon(Icons.arrow_forward, size: 32.sp, color: Colors.amber),
-        done: Text("Start", style: TextStyle(fontSize: 18.sp, color: Colors.amber)),
+        done: Text(
+          "Start",
+          style: TextStyle(fontSize: 18.sp, color: Colors.amber),
+        ),
+        dotsDecorator: const DotsDecorator(
+          activeColor: Colors.amber,
+          color: Colors.grey,
+          size: Size(8, 8),
+          activeSize: Size(22, 8),
+          activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25)),
+          ),
+        ),
         onDone: () => context.pushReplace('/quranScreen'),
       ),
     );
